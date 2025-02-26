8a64dc0d20b74e62608a90067b9baadc4a21abc8
Sanjin Dumancic <sanjin.dumancic@yahoo.com>
2025-02-26 18:48:25 +0100

    Removed zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router'
 
 import { routes } from './app.routes'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideAnimationsAsync()]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimationsAsync()
+  ]
 }
